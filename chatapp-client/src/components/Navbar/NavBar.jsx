c0b66bdfa11fbabc289d4a83f240bfcce4f51c99
Thuraitun <thuraitun64@gmail.com>
2024-04-24 13:01:31 +0630

    added logout features

--- chatapp-client/src/components/Navbar/NavBar.jsx
+++ chatapp-client/src/components/Navbar/NavBar.jsx
@@ -4,23 +4,50 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
 const NavBar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logoutUser } = useContext(AuthContext);
   return (
     <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
-        <Container>
-          <h2>
-            <Link to="/" className="link-light text-decoration-none">Chat App</Link>
-          </h2>
-          <span className="text-warning">Logged in as {user?.name}</span>
-          <Nav>
-            <Stack direction="horizontal" gap={2}>
-              <Link to="/login" className="link-light text-decoration-none">Login</Link>
-              <Link to="/register" className="link-light text-decoration-none">Register</Link>
-            </Stack>
-          </Nav>
-        </Container>
+      <Container>
+        <h2>
+          <Link to="/" className="link-light text-decoration-none">
+            Chat App
+          </Link>
+        </h2>
+        {
+          user && <span className="text-warning">Logged in as {user?.name}</span>
+        }
+        <Nav>
+          <Stack direction="horizontal" gap={2}>
+            {user && (
+              <>
+                <Link
+                  onClick={() => logoutUser()}
+                  to="/login"
+                  className="link-light text-decoration-none"
+                >
+                  Logout
+                </Link>
+              </>
+            )}
+
+            {!user && (
+              <>
+                <Link to="/login" className="link-light text-decoration-none">
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="link-light text-decoration-none"
+                >
+                  Register
+                </Link>
+              </>
+            )}
+          </Stack>
+        </Nav>
+      </Container>
     </Navbar>
   );
-}
+};
 
 export default NavBar;
--- chatapp-client/src/context/AuthContext.jsx
+++ chatapp-client/src/context/AuthContext.jsx
@@ -49,7 +49,10 @@ export const AuthContextProvider = ({ children }) => {
 
   }, [registerInfo]);
 
-  
+  const logoutUser = useCallback(() => {
+    localStorage.removeItem("User");
+    setUser(null);
+  }, [])
 
   return (
     <AuthContext.Provider
@@ -60,6 +63,7 @@ export const AuthContextProvider = ({ children }) => {
         registerUser,
         registerError,
         isRegisterLoading,
+        logoutUser
       }}
     >
       {children}
