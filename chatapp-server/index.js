8d6406e2b37adc6a2b7a8e6df223f3b9d32fe3df
Thuraitun <thuraitun64@gmail.com>
2024-04-24 21:33:57 +0630

    added Chat api

--- chatapp-server/index.js
+++ chatapp-server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./Routes/userRoute");
+const chatRoute = require("./Routes/chatRoute");
 
 const app = express();
 require("dotenv").config();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/users", userRoute);
+app.use("/api/chats",chatRoute);
 
 app.get('/', (req, res) => {
     res.send('Hello World! Welcome to my chatapp application');
